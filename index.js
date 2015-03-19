92ae623e7c24d8a12839853ad0adbf03cc6b95c0
dak0rn <daniel@suitsoft.eu>
2015-03-19 11:09:40 +0100

    A constructor function now is exported

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * espressojs - A framework for building REST APIs
+ *
+ * Copyright (c) 2015 Daniel Koch
+ *
+ * https://github.com/dak0rn/espressojs
+ */
+
+module.exports = (function() {
+
+    /**
+     * espressojs constructor function
+     */
+    var Espresso = function() {
+
+    };
+    
+    return Espresso;
+
+})();
