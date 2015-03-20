b8ac8cc6430d831de2dc0805b6c0eaf4d090f684
dak0rn <daniel@suitsoft.eu>
2015-03-20 11:45:00 +0100

    New test for exported functions

--- test/test.export.js
+++ test/test.export.js
@@ -8,4 +8,20 @@ describe('Exported value', function() {
     it('should be a function', function() {
         expect( Espresso ).to.be.a('function');
     });
+
+    describe('should have a .Request property that', function() {
+
+        it('should be a (constructor) function', function() {
+            expect( Espresso.Request ).to.be.a('function');
+        });
+
+    });
+
+    describe('should have a .Response property that', function() {
+
+        it('should be a (constructor) function', function() {
+            expect( Espresso.Response ).to.be.a('function');
+        });
+
+    });
 });
