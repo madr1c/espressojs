1db56f4401d07c65f57022cbf599950b6ae91573
dak0rn <daniel@suitsoft.eu>
2015-03-22 15:01:01 +0100

    Moved the serializer tests to an own file and added tests for execution order and default values

---
+++ test/test.Serializer.js
@@ -0,0 +1,115 @@
+/**
+ * Tests for the serializer function
+ */
+
+var expect = require('chai').expect;
+var Espresso = require('../index');
+
+describe('Serializer', function() {
+
+    it('should have a default value', function() {
+        var e = new Espresso();
+
+        expect( e._serializer ).to.be.a('function');
+    });
+
+    describe('setter', function() {
+        var iface = new Espresso();
+
+        it('should be a function', function() {
+            expect( iface.setSerializer ).to.be.a('function');
+        });
+
+        it('should take one argument', function() {
+            expect( iface.setSerializer.length ).to.equal(1);
+        });
+
+        it('should throw if no argument was given', function() {
+            var inst = new Espresso();
+            var fn = function() {
+                inst.setSerializer();
+            };
+
+            expect( fn ).to.throw(/.*/);
+        });
+    });
+
+    describe('getter', function() {
+
+        var iface = new Espresso();
+
+        it('should be a function', function() {
+            expect( iface.getSerializer ).to.be.a('function');
+        });
+
+        it('should return what .setSerializer has set', function() {
+            var inst = new Espresso();
+            var fn = function() {};
+
+            inst.setSerializer(fn);
+            expect( inst.getSerializer() ).to.equal(fn);
+        });
+
+        it('should return a default function if not set', function() {
+            expect( iface.getSerializer() ).to.be.undefined;
+        });
+    });
+
+    describe('function', function() {
+
+        it('should be invoked as the last function in the chain', function(done) {
+
+            var e = new Espresso();
+            var req = new Espresso.Request();
+            req.path = "/api/v1/users/test";
+            req.method = "get";
+            var handle = 0;
+
+            e.setSerializer( function() {
+                if( 4 !== handle )
+                    done('Failed, handle != 4');
+                else
+                    done();
+            });
+
+            e.resource('/api/:version/:collection', function() {
+                if( 2 !== handle )
+                    done('/api/:version/:collection was not called in order');
+                else
+                    handle++;
+            });
+
+            e.resource('/api/:version', function() {
+                if( 1 !== handle )
+                    done('/api/:version was not called in order');
+                else
+                    handle++;
+            });
+
+            e.resource('/api', function() {
+                if( 0 !== handle )
+                    done('/api was not called at first');
+                else
+                    handle++;
+            });
+
+            e.resource('/api/:version/:collection/:id', function() {
+                if( 3 !== handle )
+                    done('/api/:version/:collection/:id was not called in order');
+                else
+                    handle++;
+            });
+
+            var p = e.dispatchRequest(req);
+
+            p.catch( function() {
+                done('failed');
+            });
+
+        });
+
+    });
+
+
+
+});
--- test/test.interface.js
+++ test/test.interface.js
@@ -66,46 +66,6 @@ describe('Interface', function() {
             expect( df ).to.have.any.keys('promise','resolve','reject');
         });
 
-    });    
-
-    describe('.setSerializer', function() {
-
-        it('should be a function', function() {
-            expect( iface.setSerializer ).to.be.a('function');
-        });
-
-        it('should take one argument', function() {
-            expect( iface.setSerializer.length ).to.equal(1);
-        });
-
-        it('should throw if no argument was given', function() {
-            var inst = new Espresso();
-            var fn = function() {
-                inst.setSerializer();
-            };
-
-            expect( fn ).to.throw(/.*/);
-        });
     });
-
-    describe('.getSerializer', function() {
-
-        it('should be a function', function() {
-            expect( iface.getSerializer ).to.be.a('function');
-        });
-
-        it('should return what .setSerializer has set', function() {
-            var inst = new Espresso();
-            var fn = function() {};
-
-            inst.setSerializer(fn);
-            expect( inst.getSerializer() ).to.equal(fn);
-        });
-
-        it('should return a default function if not set', function() {
-            expect( iface.getSerializer() ).to.be.undefined;
-        });
-    });
-
 
 });
