a6adbc547b10004cede0669c64e443d74da17c50
dak0rn <daniel@suitsoft.eu>
2015-03-27 09:58:18 +0100

    Tests for function arguments

--- test/test.delete.js
+++ test/test.delete.js
@@ -14,4 +14,22 @@ describe('Espresso.prototype.delete', function() {
         expect( new Espresso().delete.length ).to.equal(1);
     });
 
+    it('should throw if an invalid argument is given', function(){
+
+        var e = new Espresso();
+
+        [ {}, null, undefined, 42, 'string', false ].forEach( function(item) {
+            expect( function(){ e.delete(item); } ).to.throw(/object/);
+        });
+
+        [ { name: '' }, { id: '' }, { pattern: '' } ].forEach( function(item) {
+            expect( function(){ e.delete(item); } ).to.throw(/empty/);
+        });
+
+        [ { name: 'name' }, { id: 'id' }, { pattern: 'pattern' } ].forEach( function(item) {
+            expect( function(){ e.delete(item); } ).not.to.throw(/object/);
+        });
+
+    });
+
 });
