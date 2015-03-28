79b85e58ab4dc5b75f5f4e24a5db67270f226c1b
dak0rn <daniel@suitsoft.eu>
2015-03-28 10:49:18 +0100

    Tests for handler.register

--- test/test.utils.js
+++ test/test.utils.js
@@ -46,4 +46,66 @@ describe('Utils', function() {
         });
     });
 
+    // .handler
+    describe('.handler', function() {
+
+        it('should be an object', function(){
+            expect(utils.handler).to.be.an('object');
+        });
+
+        describe('.register', function() {
+            it('should be a function', function(){
+                expect( utils.handler.register ).to.be.a('function');
+            });
+
+            it('should take two arguments', function() {
+                expect( utils.handler.register.length ).to.equal(2);
+            });
+
+            it('should add a handler to _ids', function() {
+
+                var e = new Espresso();
+                var h = new Espresso.Handler( '/:what', function() {});
+
+                expect( _.keys(e._ids).length ).to.equal(0);
+
+                utils.handler.register(h);
+
+                expect( _.keys(e._ids).length ).to.equal(1);
+                expect( e._ids[ h.getExpression() ] ).to.equal(h);
+
+            });
+
+            it('should add a handler to _names if there is a name', function() {
+
+                var e = new Espresso();
+                var h = new Espresso.Handler( '/:what', function() {});
+                var s = new Espresso.Handler( '/:what', function() {}, {name:'s'});
+
+                expect( _.keys(e._names).length ).to.equal(0);
+
+                utils.handler.register(h);
+                utils.handler.register(s);
+
+                expect( _.keys(e._names).length ).to.equal(1);
+                expect( e._names.s ).to.equal(s);
+
+            });
+
+            it('must not update the resources table', function() {
+
+                var e = new Espresso();
+                var h = new Espresso.Handler( '/:what', function() {});
+
+                expect( _.keys(e._resources).length ).to.equal(0);
+
+                utils.handler.register(h);
+
+                expect( _.keys(e._resources).length ).to.equal(0);
+
+            });
+
+        });
+    });
+
 });
